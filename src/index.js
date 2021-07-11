ed275677b8627d378467e0435814a3719d44c253
sae-github <miyasae1126@gmail.com>
2021-07-11 09:46:47 +0900

    update:index.html index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const js_hamburger = document.getElementById("js-hamburger");
-const js_globalNav = document.getElementById("js-global-nav");
+const hamburger = document.getElementById("js-hamburger");
+const globalNav = document.getElementById("js-global-nav");
 
-js_hamburger.addEventListener("mouseover",function() {
-  js_globalNav.style.willChange = "transform";
+hamburger.addEventListener("mouseover",function() {
+  globalNav.style.willChange = "transform";
 });
-js_hamburger.addEventListener("mouseout",function() {
-  js_globalNav.style.willChange = "";
+hamburger.addEventListener("mouseout",function() {
+  globalNav.style.willChange = "";
 });
 
-js_hamburger.addEventListener("click",function () {
-  js_globalNav.classList.toggle("is-active");
-  if (this.getAttribute("aria-expanded") === "false") {
+hamburger.addEventListener("click",function () {
+  globalNav.classList.toggle("is-active");
+  if (globalNav.classList.contains("is-active")) {
     this.setAttribute("aria-expanded", "true");
-    js_globalNav.setAttribute("aria-hidden", "false");
+    globalNav.setAttribute("aria-hidden", "false");
   } else {
     this.setAttribute("aria-expanded", "false");
-    js_globalNav.setAttribute("aria-hidden", "true");
+    globalNav.setAttribute("aria-hidden", "true");
   }
 });
